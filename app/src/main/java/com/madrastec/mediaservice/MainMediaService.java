6ba75cde475e88f129cf3d8480101c350adad753
Yogesh Soundrapandian <yogeshsoundrapandian@gmail.com>
2020-11-09 17:57:18 +0530

    changes

--- app/src/main/java/com/madrastec/mediaservice/MainMediaService.java
+++ app/src/main/java/com/madrastec/mediaservice/MainMediaService.java
@@ -2,7 +2,10 @@ package com.madrastec.mediaservice;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.os.Environment;
+
 import java.io.File;
+import java.io.FilenameFilter;
 import java.util.ArrayList;
 
 public class MainMediaService {
@@ -91,6 +94,30 @@ public class MainMediaService {
 
         public ArrayList<Bitmap> getImageBitmap() {
             return  imageBitmap;
+        }
+    }
+
+    public static class FileRetriever {
+
+        public static File[] fileRetriever(String mFolder, final String[] extension){
+
+            File[] files = null;
+            File folder = new File(Environment.getExternalStorageDirectory() + File.separator + mFolder);
+
+            if (folder.exists()) {
+                files = folder.listFiles(new FilenameFilter() {
+                    public boolean accept(File dir, String name) {
+                        for (String s : extension) {
+
+                            if (name.endsWith(s))
+                                return true;
+                        }
+                        return false;
+                    }
+                });
+            }
+
+            return files;
         }
     }
 }
