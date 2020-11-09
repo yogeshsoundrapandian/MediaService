f0e37d9dd5b6e150a0eb73fc094e163a9fa62d12
Yogesh Soundrapandian <yogeshsoundrapandian@gmail.com>
2020-11-09 18:16:40 +0530

    changes

--- app/src/main/java/com/madrastec/mediaservice/MainActivity.java
+++ app/src/main/java/com/madrastec/mediaservice/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import java.io.File;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +12,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        String[] sample = new String[]{"a","b"};
     }
 }
