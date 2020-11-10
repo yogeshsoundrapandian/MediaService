272c7f25623d0b31d4469b34ab17e3a3efe2eb27
Yogesh Soundrapandian <yogeshsoundrapandian@gmail.com>
2020-11-11 01:53:10 +0530

    changes

--- MediaService/src/main/java/com/madrastec/mediaservice/MediaService.java
+++ MediaService/src/main/java/com/madrastec/mediaservice/MediaService.java
@@ -1,15 +1,8 @@
 package com.madrastec.mediaservice;
 
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.media.ThumbnailUtils;
-import android.os.Environment;
-import android.provider.MediaStore;
 
-import java.io.File;
-import java.io.FilenameFilter;
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public class MediaService {
 
@@ -83,55 +76,4 @@ public class MediaService {
         }
     }
 
-    public static class FileRetriever {
-
-        ArrayList<String> fileList = new ArrayList<>();
-        ArrayList<String> filePath = new ArrayList<>();;
-        ArrayList<Bitmap> fileBitmap = new ArrayList<>();
-
-        File[] files;
-
-        public FileRetriever(String mFolder, final String[] extension){
-
-            ArrayList<String> extensionList = new ArrayList<String>(Arrays.asList(extension));
-
-            File folder = new File(Environment.getExternalStorageDirectory() + File.separator + mFolder);
-
-            if (folder.exists()) {
-                files = folder.listFiles(new FilenameFilter() {
-                    public boolean accept(File dir, String name) {
-                        for (String s : extension) {
-
-                            if (name.endsWith(s))
-                                return true;
-                        }
-                        return false;
-                    }
-                });
-            }
-
-            assert files != null;
-            for (File file : files) {
-                fileList.add(file.getName());
-                filePath.add(file.getPath());
-                if ( extensionList.contains(".mp4") || extensionList.contains(".mpeg4") ||
-                        extensionList.contains(".mp3") || extensionList.contains(".aac") )
-                    fileBitmap.add(ThumbnailUtils.createVideoThumbnail(file.getAbsolutePath(), MediaStore.Video.Thumbnails.MINI_KIND));
-                else
-                    fileBitmap.add(BitmapFactory.decodeFile(file.getPath()));
-            }
-        }
-
-        public ArrayList<String> getFileList() {
-            return  fileList;
-        }
-
-        public ArrayList<String> getFilePath() {
-            return  filePath;
-        }
-
-        public ArrayList<Bitmap> getFileBitmap() {
-            return  fileBitmap;
-        }
-    }
 }
